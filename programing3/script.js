b0e433ac0275d66b0fe8af502dba131712863c93
TatevOhanjanyan <tatev.ohanjanyan.s@tumo.org>
2022-10-30 13:20:23 +0400

    my 6-th commit

--- programing3/script.js
+++ programing3/script.js
@@ -167,6 +167,18 @@ function draw() {
         }
             socket.emit("send static", statistic);
     }
+
+    const btn = document.querySelector('button');
+
+    function random(number) {
+      return Math.floor(Math.random() * (number+1));
+    }
     
+    btn.addEventListener('click', () => {
+      const rndCol = `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+      document.body.style.backgroundColor = rndCol;
+    });
+
+   
 }
 
